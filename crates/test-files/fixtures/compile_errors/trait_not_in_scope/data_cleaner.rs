327d63733131ffb4c4db98e492d897e8d2f5b0bc
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-01 22:52:07 +0000

    Add data cleaning utility to remove null values and deduplicate rows

---
+++ crates/test-files/fixtures/compile_errors/trait_not_in_scope/data_cleaner.rs
@@ -0,0 +1,62 @@
+use std::collections::HashSet;
+
+pub struct DataCleaner<T> {
+    data: Vec<Vec<T>>,
+}
+
+impl<T> DataCleaner<T>
+where
+    T: Clone + Eq + std::hash::Hash,
+{
+    pub fn new(data: Vec<Vec<T>>) -> Self {
+        DataCleaner { data }
+    }
+
+    pub fn remove_null_rows(&mut self, null_value: &T) {
+        self.data.retain(|row| !row.iter().any(|cell| cell == null_value));
+    }
+
+    pub fn deduplicate_rows(&mut self) {
+        let mut seen = HashSet::new();
+        self.data.retain(|row| seen.insert(row.clone()));
+    }
+
+    pub fn get_data(&self) -> &Vec<Vec<T>> {
+        &self.data
+    }
+
+    pub fn into_data(self) -> Vec<Vec<T>> {
+        self.data
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_remove_null_rows() {
+        let data = vec![
+            vec!["A", "B", "C"],
+            vec!["null", "E", "F"],
+            vec!["G", "H", "null"],
+            vec!["I", "J", "K"],
+        ];
+        let mut cleaner = DataCleaner::new(data);
+        cleaner.remove_null_rows(&"null");
+        assert_eq!(cleaner.get_data().len(), 2);
+    }
+
+    #[test]
+    fn test_deduplicate_rows() {
+        let data = vec![
+            vec![1, 2, 3],
+            vec![4, 5, 6],
+            vec![1, 2, 3],
+            vec![7, 8, 9],
+        ];
+        let mut cleaner = DataCleaner::new(data);
+        cleaner.deduplicate_rows();
+        assert_eq!(cleaner.get_data().len(), 3);
+    }
+}
