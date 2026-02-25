4611dd8a9752a143a38fac4cf5409629adf89250
FelixMakes <felix.makes@hotmail.com>
2026-02-25 22:52:11 +0000

    Implement a basic WebSocket echo server using tokio-tungstenite

--- crates/test-files/fixtures/compile_errors/bad_ingot/src/websocket_server.rs
+++ crates/test-files/fixtures/compile_errors/bad_ingot/src/websocket_server.rs
@@ -7,7 +7,7 @@ use tokio_tungstenite::accept_async;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "127.0.0.1:8080";
     let listener = TcpListener::bind(addr).await?;
-    println!("WebSocket server listening on ws://{}", addr);
+    println!("WebSocket echo server listening on {}", addr);
 
     while let Ok((stream, _)) = listener.accept().await {
         tokio::spawn(async move {
@@ -21,66 +21,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 async fn handle_connection(stream: tokio::net::TcpStream) -> Result<(), Box<dyn std::error::Error>> {
     let ws_stream = accept_async(stream).await?;
-    let (mut write, mut read) = ws_stream.split();
+    let (mut sender, mut receiver) = ws_stream.split();
 
-    while let Some(msg) = read.next().await {
-        let msg = msg?;
-        if msg.is_close() {
-            break;
-        }
-        if msg.is_text() || msg.is_binary() {
-            write.send(msg).await?;
-        }
-    }
-    Ok(())
-}
-use futures_util::{SinkExt, StreamExt};
-use tokio::net::TcpListener;
-use tokio_tungstenite::accept_async;
-use tokio_tungstenite::tungstenite::Message;
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "127.0.0.1:8080";
-    let listener = TcpListener::bind(addr).await?;
-    println!("WebSocket server listening on {}", addr);
-
-    while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(handle_connection(stream));
-    }
-
-    Ok(())
-}
-
-async fn handle_connection(stream: tokio::net::TcpStream) {
-    let ws_stream = match accept_async(stream).await {
-        Ok(ws) => ws,
-        Err(e) => {
-            eprintln!("Error during WebSocket handshake: {}", e);
-            return;
-        }
-    };
-
-    let (mut write, mut read) = ws_stream.split();
-
-    while let Some(msg) = read.next().await {
+    while let Some(msg) = receiver.next().await {
         match msg {
             Ok(Message::Text(text)) => {
                 println!("Received text message: {}", text);
-                if let Err(e) = write.send(Message::Text(text)).await {
-                    eprintln!("Error sending message: {}", e);
-                    break;
-                }
+                sender.send(Message::Text(text)).await?;
             }
             Ok(Message::Close(_)) => {
                 println!("Client disconnected");
                 break;
             }
+            Ok(other) => {
+                println!("Received other message type: {:?}", other);
+            }
             Err(e) => {
-                eprintln!("Error reading message: {}", e);
+                eprintln!("Error receiving message: {}", e);
                 break;
             }
-            _ => {}
         }
     }
+    Ok(())
 }
